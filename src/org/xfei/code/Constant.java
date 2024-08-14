b7224da78a269d58cfc7d8ef164bc607cf9b2e07
SummitCore831 <quinnjace984@gmail.com>
2024-08-14 14:33:15 +0300

    Update Constant.java

--- src/org/xfei/code/Constant.java
+++ src/org/xfei/code/Constant.java
@@ -5,20 +5,21 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class Constant {
-	public static final String PATH;
-	static{
-		
-		Properties properties = new Properties();
-		String path = new File(".").getAbsolutePath();
-		try {
-			properties.load(Thread.currentThread().getContextClassLoader().getResourceAsStream("../config.properties"));
-			path=properties.getProperty("path");
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			System.out.println("Using default path: "+path);
-		}
-		PATH=path;
 
-	}
+    public static final String PATH;
+
+    static {
+        Properties properties = new Properties();
+        String path = new File(".").getAbsolutePath();
+
+        try {
+            properties.load(Thread.currentThread().getContextClassLoader().getResourceAsStream("../config.properties"));
+            path = properties.getProperty("path");
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Using default path: " + path);
+        }
+
+        PATH = path;
+    }
 }
